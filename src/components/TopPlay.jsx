e12dd0c6d92efb4ababf8f53b8dfd1cc883010d4
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-05-16 22:06:37 -0700

    Added to TopPlay component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,7 +11,23 @@ import "swiper/css/free-mode";
 
 const TopChartCard = ({ song, i }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.attributes.name}
+    <h3 className="flex flex-row items-center text-white mr-4">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img
+        className="w-20 h-20 rounded-lg mr-2"
+        src={song?.attributes.artwork.url}
+      />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/song${song?.id}`}>
+          <p className="text-xl font-bold text-white">{song.attributes.name}</p>
+        </Link>
+        <Link to={`/artists/song${song?.artistName}`}>
+          <p className="text-base font-400 text-gray-300 mt-1">
+            {song.attributes.artistName}
+          </p>
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
